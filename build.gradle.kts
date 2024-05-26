759295fea1edeff688eae0f33b1fab02eeb3b104
Fred Zhen <fred.zhen@gmail.com>
2024-05-26 14:53:38 +0800

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.6"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "guru.ysy"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+extra["springAiVersion"] = "0.8.1"
+extra["openAPIVersion"] = "2.5.0"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    // mistral ai
+    implementation("org.springframework.ai:spring-ai-mistral-ai-spring-boot-starter")
+    // OpenAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("openAPIVersion")}")
+    // lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    // lombok for test
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // explicit declare JUnit5 for testing ready for gradle 9
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+
+        showExceptions = true
+        setExceptionFormat("full")
+        showCauses = true
+        showStackTraces = true
+
+        showStandardStreams = true // show more verbose test output
+    }
+    // avoid dynamic agent loading warning for JDK21
+    jvmArgs("-XX:+EnableDynamicAgentLoading")
+}
---
+++ src/main/java/guru/ysy/aifunc/AiFuncApplication.java
@@ -0,0 +1,13 @@
+package guru.ysy.aifunc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFuncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiFuncApplication.class, args);
+    }
+
+}
